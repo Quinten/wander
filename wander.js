bd733e9f2e8ef3ca124363a08d8b17a348ac311e
Quinten Clause <quintenclause@hotmail.com>
2016-02-20 11:04:55 +0100

    Added basics of vector lib

--- wander.js
+++ wander.js
@@ -1,12 +1,15 @@
 w=a.width,h=a.height;
-m=Math,r=m.random;q=[];
-for(i=0;i<99;i++){q[i]={x:r()*w,y:r()*h}}
+M=Math,R=M.random;
+V=function (x,y) {this.x=x;this.y=y;
+this.a=function(v){this.x+=v.x;this.y+=v.y}};
+q=[];for(i=0;i<99;i++){q[i]={p:new V(R()*w,R()*h),v:new V(1,1)}};
 function f(){
 c.fillStyle='#ccc';c.fillRect(0,0,w,h);c.fillStyle='#fff';
-for(i in q){p=q[i];x=p.x;y=p.y;
-x+=1;y+=1;
+for(i in q){o=q[i];
+o.p.a(o.v);
+x=o.p.x;y=o.p.y;
 x=(x>w)?0:(x<0)?w:x;
 y=(y>h)?0:(y<0)?h:y;
 c.fillRect(x,y,2,2);
-p.x=x;p.y=y;
+o.p.x=x;o.p.y=y;
 }requestAnimationFrame(f);}f();
